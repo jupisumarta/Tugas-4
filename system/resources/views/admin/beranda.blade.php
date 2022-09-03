29bf4eaca3a3db08c2e90cd9c40944b653218b05
jupisumarta <joops089663@gmail.com>
2022-09-02 17:48:27 -0700

    Tugas-4
    Mengerjakan Tugas-4

---
+++ system/resources/views/admin/beranda.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.base')
+
+@section('content')
+    <h1>Manusia Konyol</h1>
+    <img  style="height: 300px; width: 300px;" src="{{url('public')}}/dist/img/IMG_20190314_215923_439.jpg">
+@endsection
---
+++ system/routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\ClientController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('admin', function() {
+    return view("admin.base");
+});
+
+
+Route::get('beranda', [HomeController::class, 'showBeranda']);
+Route::get('kategori', [HomeController::class, 'showKategori']);
+Route::get('login', [AuthController::class, 'showLogin']);
+Route::get('pelanggan', [HomeController::class, 'showPelanggan']);
+Route::get('supplier', [HomeController::class, 'showSupplier']);
+Route::get('promo', [HomeController::class, 'showPromo']);
+
+
+Route::get('produk', [ProdukController::class, 'index']);
+Route::get('produk/create', [ProdukController::class, 'create']);
+Route::post('produk', [ProdukController::class, 'store']);
+Route::get('produk/{produk}', [ProdukController::class, 'show']);
+Route::get('produk/{produk}/edit', [ProdukController::class, 'edit']);
+Route::put('produk/{produk}', [ProdukController::class, 'update']);
+Route::delete('produk/{produk}', [ProdukController::class, 'destroy']);
+
+// frontview
+Route::get('cart', [ClientController::class, 'showCart']);
+Route::get('shop', [ClientController::class, 'showShop']);
+Route::get('shop-produk', [ClientController::class, 'showShopproduk']);
